4aaecc3ad84a2cbbb8ff56ecac9e3793694242ac
LYSHIXD_MAC <lyshixd@gmail.com>
2020-05-05 12:56:27 +0800

    first submit

---
+++ app/src/main/java/com/lyshixd/dellwithfastclick/GeneralClickListener.java
@@ -0,0 +1,25 @@
+package com.lyshixd.dellwithfastclick;
+
+import android.view.View;
+
+public abstract class GeneralClickListener implements View.OnClickListener {
+
+	private String mActivitySimpleName;
+
+	public GeneralClickListener() {
+
+	}
+
+	public GeneralClickListener(String mActivitySimpleName) {
+		this.mActivitySimpleName = mActivitySimpleName;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (!FastClickUtil.isFastClick(mActivitySimpleName)) {
+			onDellWithFastClick();
+		}
+	}
+
+	protected abstract void onDellWithFastClick();
+}
